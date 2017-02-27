a166e057289552f715b8c0d93df9e77572c5cd5a
Nick A. Knowles <knowlen@LabImage2016-17>
2017-02-26 16:14:10 -0800

    added comments

--- Bash/bash_recursion_eg.sh
+++ Bash/bash_recursion_eg.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+
+# rec
+#  Takes an input, then does a recursive subtraction of 
+#  1 until that input is 0.
+#
 rec (){
     if [ "$1" == "0" ]; then
         exit;
@@ -10,6 +15,10 @@ rec (){
 
 }
 
+# trying
+#  prompts the user for input, then either exits or 
+#  calls itself again depending on that input. 
+#
 trying (){
     echo "go?"
     read x
@@ -19,7 +28,6 @@ trying (){
     else
         exit
     fi
-
 }
 
 
